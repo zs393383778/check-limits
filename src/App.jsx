567dcc45d0c231cfdf4173489c94e4776426a256
zhang <393383778@qq.com>
2019-07-11 15:49:11 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button>2</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
