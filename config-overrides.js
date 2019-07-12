0d57a1b6fd01044601ae94ada6504507ed1c4555
zhang <393383778@qq.com>
2019-07-12 14:19:31 +0800

    置rem，babel，按需引入ant

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,17 @@
 const {
   override,
-  fixBabelImports
+  fixBabelImports,
+  addPostcssPlugins
 } = require('customize-cra');
 
+
 module.exports = override(
+  addPostcssPlugins([require("postcss-px2rem")({
+    remUnit: 32
+  })]),
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
     style: 'css',
-  }),
+  })
 );
--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
+import "./rem.js"
 
 ReactDOM.render( < App / > , document.getElementById("root"));
